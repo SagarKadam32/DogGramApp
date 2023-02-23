dfb99369f6b4207002a1bf7ff4536fe3ab1cc25e
Sagar Kadam <misc.sagarkadam@gmail.com>
2023-02-23 18:59:09 +0530

    Initial Commit

---
+++ DogGramApp/DogGramAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DogGramAppApp.swift
+//  DogGramApp
+//
+//  Created by Sagar Kadam on 23/02/23.
+//
+
+import SwiftUI
+
+@main
+struct DogGramAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
