e82f07b1b5d054a6421d446920292cdf50c116b9
Sagar Kadam <misc.sagarkadam@gmail.com>
2023-02-26 19:58:11 +0530

    Added PostPicture View.

---
+++ DogGramApp/Views/Screens/PostImageView.swift
@@ -0,0 +1,79 @@
+//
+//  PostImageView.swift
+//  DogGramApp
+//
+//  Created by Sagar Kadam on 26/02/23.
+//
+
+import SwiftUI
+
+struct PostImageView: View {
+    
+    @Environment(\.presentationMode) var presentationMode
+    @State var captionText: String = ""
+    
+    var body: some View {
+        VStack(alignment: .center, spacing: 0) {
+            
+            HStack {
+                Button(action: {
+                    presentationMode.wrappedValue.dismiss()
+                }, label: {
+                    Image(systemName: "xmark")
+                        .font(.title)
+                        .padding()
+                })
+                .accentColor(.primary)
+                
+                Spacer()
+            }
+            
+            ScrollView(.vertical, showsIndicators: false) {
+                Image("dog1")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 200, height: 200, alignment: .center)
+                    .cornerRadius(12)
+                    .clipped()
+                
+                TextField("Add your caption here..", text: $captionText)
+                    .padding()
+                    .frame(height: 60)
+                    .frame(maxWidth: .infinity)
+                    .background(Color.MyTheme.beigeColor)
+                    .cornerRadius(12)
+                    .font(.headline)
+                    .padding(.horizontal)
+                    .autocapitalization(.sentences)
+                
+                
+                Button(action: {
+                    postPicture()
+                }, label: {
+                    Text("Post Picture!".uppercased())
+                        .font(.title3)
+                        .fontWeight(.bold)
+                        .padding()
+                        .frame(height: 60)
+                        .frame(maxWidth: .infinity)
+                        .background(Color.MyTheme.purpuleColor)
+                        .cornerRadius(12)
+                        .padding(.horizontal)
+                })
+                .accentColor(Color.MyTheme.yellowColor)
+             }
+        }
+    }
+    
+    // MARK: FUNCTIONS
+    
+    func postPicture() {
+        print("POST PICTURE TO DATABASE HERE..")
+    }
+}
+
+struct PostImageView_Previews: PreviewProvider {
+    static var previews: some View {
+        PostImageView()
+    }
+}
