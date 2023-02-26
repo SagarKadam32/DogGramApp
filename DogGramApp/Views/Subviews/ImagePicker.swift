a37f2b5cf267010bebd2690ef2536b12966a6d67
Sagar Kadam <misc.sagarkadam@gmail.com>
2023-02-26 13:50:20 +0530

    Added the code for ImagePicker

---
+++ DogGramApp/Views/Subviews/ImagePicker.swift
@@ -0,0 +1,53 @@
+//
+//  ImagePicker.swift
+//  DogGramApp
+//
+//  Created by Sagar Kadam on 26/02/23.
+//
+
+import Foundation
+import SwiftUI
+
+struct ImagePicker: UIViewControllerRepresentable {
+    
+    @Environment(\.presentationMode) var presentationMode
+    @Binding var imageSelected: UIImage
+    @Binding var sourceType: UIImagePickerController.SourceType
+    
+    func makeUIViewController(context: UIViewControllerRepresentableContext<ImagePicker>) -> some UIImagePickerController {
+        let picker = UIImagePickerController()
+        picker.delegate = context.coordinator
+        picker.sourceType = sourceType
+        picker.allowsEditing = true
+        return picker
+    }
+    
+    func updateUIViewController(_ uiViewController: UIViewControllerType, context:  UIViewControllerRepresentableContext<ImagePicker>) {}
+    
+    func makeCoordinator() -> ImagePickerCoordinator {
+        return ImagePickerCoordinator(parent: self)
+    }
+    
+    class ImagePickerCoordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+        
+        let parent: ImagePicker
+        
+        init(parent: ImagePicker) {
+            self.parent = parent
+        }
+        
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+            
+            if let image = info[.editedImage] as? UIImage ?? info[.originalImage] as? UIImage  {
+                
+                // Select Image for our App
+                parent.imageSelected = image
+                
+                // Dismiss the screen
+                parent.presentationMode.wrappedValue.dismiss()
+                
+            }
+        }
+    }
+    
+}
